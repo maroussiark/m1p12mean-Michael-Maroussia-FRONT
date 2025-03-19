d8686a4fd841a12f6ad9dec39af2910b08689996
maroussia <soamaroussia@gmail.com>
2025-03-19 10:35:24 +0300

    rebase-2

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app.config';
+import { AppComponent } from './app.component';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
-bootstrap().catch((err) => console.log(err));
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
